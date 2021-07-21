ab81666fb26b1ec7e47ca9106a13fe7683c34c32
WONCHAN BAE <baewonchan1@naver.com>
2021-07-21 16:21:57 +0900

    Make see profile and edit profile api

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,12 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.db.models import fields
 from rest_framework import serializers
-from users.serializers import UserSerializer
+from users.serializers import RelatedUserSerializer
 from .models import Room
 
 class ReadRoomSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer()
+    user = RelatedUserSerializer()
     class Meta:
         model = Room
         exclude = ("modified",)
