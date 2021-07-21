c6c41abc2c10f03a8669b9eb85d87159a7adff79
WONCHAN BAE <baewonchan1@naver.com>
2021-07-21 19:02:37 +0900

    Make add and delete favs api

--- users/views.py
+++ users/views.py
@@ -2,11 +2,13 @@ from functools import partial
 from rooms import serializers
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView, set_rollback
+from rest_framework.views import APIView
 from .models import User
+from rooms.models import Room
 from .serializers import ReadUserSerializer, WriteUserSerializer
+from rooms.serializers import RoomSerializer
 
 # Create your views here.
 class MeView(APIView):
@@ -32,3 +34,28 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data)
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    def put(self, request):
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
